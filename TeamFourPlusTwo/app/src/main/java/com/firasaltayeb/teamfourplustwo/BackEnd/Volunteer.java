88fd263379647e48aeb8cbce07e09157616bd794
firas altayeb <firas.altayeb@kcl.ac.uk>
2015-12-04 23:07:56 +0000

    updated the volunteer and elderly classes

--- TeamFourPlusTwo/app/src/main/java/com/firasaltayeb/teamfourplustwo/BackEnd/Volunteer.java
+++ TeamFourPlusTwo/app/src/main/java/com/firasaltayeb/teamfourplustwo/BackEnd/Volunteer.java
@@ -1,7 +1,46 @@
 package com.firasaltayeb.teamfourplustwo.BackEnd;
 
+import java.util.ArrayList;
+
 /**
  * Created by firasAltayeb on 04/12/2015.
  */
+
 public class Volunteer {
+    private String name;
+    private String interest;
+    private int age;
+    private ArrayList<String> blackList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public void setInterest(String interest) {
+        this.interest = interest;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public ArrayList<String> getBlackList() {
+        return blackList;
+    }
+
+    public void setBlackList(ArrayList<String> blackList) {
+        this.blackList = blackList;
+    }
 }
