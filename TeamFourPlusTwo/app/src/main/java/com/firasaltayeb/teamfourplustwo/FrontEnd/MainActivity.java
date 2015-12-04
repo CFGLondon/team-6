d0f72d61b990c338c05b3cfa09d226e4a484468e
firas altayeb <firas.altayeb@kcl.ac.uk>
2015-12-04 23:44:17 +0000

    added a thrid activity

--- TeamFourPlusTwo/app/src/main/java/com/firasaltayeb/teamfourplustwo/FrontEnd/MainActivity.java
+++ TeamFourPlusTwo/app/src/main/java/com/firasaltayeb/teamfourplustwo/FrontEnd/MainActivity.java
@@ -11,6 +11,7 @@ import com.firasaltayeb.teamfourplustwo.R;
 public class MainActivity extends AppCompatActivity {
 
     public Button volunteer;
+    public Button elderly;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         volunteer = (Button) findViewById(R.id.btnVolunteer);
-
+        elderly = (Button) findViewById(R.id.btnElderly);
     }
 
 
     public void startVolunteerActivity(View view) {
         Intent intent = new Intent(this, VolunteerActivity.class);
+        startActivity(intent);
+    }
+
+    public void startElderlyActivity(View view) {
+        Intent intent = new Intent(this, ElderlyActivity.class);
         startActivity(intent);
     }
 }
