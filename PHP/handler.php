fe77c36d7cfe078d77715b4209983a164de4025b
David Wells <dwlpl23@hotmail.co.uk>
2015-12-04 22:53:40 +0000

    Fixed PHP file

--- PHP/handler.php
+++ PHP/handler.php
@@ -4,7 +4,7 @@
   $phone = $_REQUEST['phone'];
   $likes = $_REQUEST['likes'];
   $dislikes = $_REQUEST['dislikes'];
-  $daysArray = $_REQUEST['days'];  //Need to decide how elements will be stored in the database
+  $daysArray[] = $_REQUEST['days'];  //Need to decide how elements will be stored in the database
 
   $host = 'ec2-54-78-38-127.eu-west-1.compute.amazonaws.com'
   $username = 'ubuntu'
@@ -16,16 +16,14 @@
   	die('Could not connect to the database.  Error ' . $db->connect_error);
   }
 
-  $sql = "INSERT INTO 'table'
-  		  VALUES ('$name','$email','$phone','$likes','$dislikes','$daysArray')";
+  $implodedDays = implode(', ', $daysArray);
+
+  $sql = "INSERT INTO 'table' VALUES ($name,$email,$phone,$likes,$dislikes,$implodedDays)";
 
 if(!$result = $db->query($sql)){
   	die('Error running query: ' . $db->error);
   	return false;
-  else{
+  }
+else{
   	return true;
   }
-}
-
-
-?>
