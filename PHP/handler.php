6bd9d9548286a78f31342c4592e440c165da5b50
David Wells <dwlpl23@hotmail.co.uk>
2015-12-04 21:09:29 +0000

    Updated PHP handler

--- PHP/handler.php
+++ PHP/handler.php
@@ -1,7 +1,31 @@
 <?php
-  if($_SERVER['']){
+  $name = $_REQUEST['name'];
+  $email = $_REQUEST['email'];
+  $phone = $_REQUEST['phone'];
+  $likes = $_REQUEST['likes'];
+  $dislikes = $_REQUEST['dislikes'];
+
+  $host = ''
+  $dbName = ''
+  $username = ''
+  $password = ''
+
+  $db = new mysqli($host,$username,$password,$dbName);
+
+  if($db->connect_Errno > 0){
+  	die('Could not connect to the database.  Error ' . $db->connect_error);
+  }
+
+  $sql = "INSERT INTO 'table'
+  		  VALUES ('$name','$email','$phone','$likes','$dislikes')";
+
+if(!$result = $db->query($sql)){
+  	die('Error running query: ' . $db->error);
+  	return false;
+  else{
   	return true;
   }
+}
 
 
 ?>
