7f71d145a97454b7182349a5bb3e09829a77e0d6
firas altayeb <firas.altayeb@kcl.ac.uk>
2015-12-04 23:35:59 +0000

    added a secound activity

--- TeamFourPlusTwo/app/src/main/java/com/firasaltayeb/teamfourplustwo/FrontEnd/MainActivity.java
+++ TeamFourPlusTwo/app/src/main/java/com/firasaltayeb/teamfourplustwo/FrontEnd/MainActivity.java
@@ -1,16 +1,29 @@
 package com.firasaltayeb.teamfourplustwo.FrontEnd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.firasaltayeb.teamfourplustwo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button volunteer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        volunteer = (Button) findViewById(R.id.btnVolunteer);
+
     }
 
+
+    public void startVolunteerActivity(View view) {
+        Intent intent = new Intent(this, VolunteerActivity.class);
+        startActivity(intent);
+    }
 }
