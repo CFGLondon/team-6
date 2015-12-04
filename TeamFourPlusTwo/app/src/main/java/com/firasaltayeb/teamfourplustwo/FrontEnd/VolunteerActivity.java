8631c264ed7ac366b462b70a4953517276020751
firas altayeb <firas.altayeb@kcl.ac.uk>
2015-12-04 23:26:25 +0000

    added inital xml files

---
+++ TeamFourPlusTwo/app/src/main/java/com/firasaltayeb/teamfourplustwo/FrontEnd/VolunteerActivity.java
@@ -0,0 +1,15 @@
+package com.firasaltayeb.teamfourplustwo.FrontEnd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firasaltayeb.teamfourplustwo.R;
+
+public class VolunteerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer);
+    }
+}
