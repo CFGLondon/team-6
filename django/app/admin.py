4ffb8118bc39229f9d6fa1a415c3d8fb529c40f3
amld-code4good <nyr235@gmail.com>
2015-12-05 02:56:23 +0000

    model updates

--- django/app/admin.py
+++ django/app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from app.models import Constituent
+from app.models import Constituent, Volunteer
 
 admin.site.register(Constituent)
+admin.site.register(Volunteer)
--- django/app/models.py
+++ django/app/models.py
@@ -3,8 +3,26 @@ from django.contrib.auth.models import User
 
 class Constituent(models.Model):
 	user = models.OneToOneField(User)
+	address = models.CharField(max_length=500)
+	interests = models.CharField(max_length=500)
+	black = models.CharField(max_length=500)
+	white = models.CharField(max_length=500)
+	#date = models.DateField()
 	
-	
+	def __unicode__(self):
+		return self.name
+
+class Volunteer(models.Model):
+	user = models.OneToOneField(User)
+	address = models.CharField(max_length=500)
+	interests = models.CharField(max_length=500)
+	black = models.CharField(max_length=500)
+	white = models.CharField(max_length=500)
+	#date = models.DateField()
+	reference1number = models.IntegerField()
+	reference1name = models.CharField(max_length=255)
+	reference2number = models.IntegerField()	
+	reference2name = models.CharField(max_length=255)
 
 	def __unicode__(self):
 		return self.name
