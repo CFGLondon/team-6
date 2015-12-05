52ae6f56116062d52e5e35022807dcd037b03dd7
Rivaan <ran.rana@kcl.ac.uk>
2015-12-05 00:50:35 +0000

    very revised handler file, using PDO for database actions

--- PHP/handler.php
+++ PHP/handler.php
@@ -1,31 +1,58 @@
 <?php
-  $name = $_REQUEST['name'];
-  $email = $_REQUEST['email'];
-  $phone = $_REQUEST['phone'];
-  $likes = $_REQUEST['likes'];
-  $dislikes = $_REQUEST['dislikes'];
-  $daysArray = $_REQUEST['days'];  //Need to decide how elements will be stored in the database
 
-  $host = 'ec2-54-78-38-127.eu-west-1.compute.amazonaws.com'
-  $username = 'root'
-  $password = 'code4good'
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $address = $_POST['address'];
+  $interests = $_POST['interests'];
+  $black = $_POST['black'];
+  $white = $_POST['white'];
+  $insert = false;
+//  $daysArray[] = $_POST['days'];  //Need to decide how elements will be stored in the database
 
-  $db = new mysqli($host,$username,$password);
-
-  if($db->connect_Errno > 0){
-  	die('Could not connect to the database.  Error ' . $db->connect_error);
+  try {
+      $host = 'http://ec2-54-78-38-127.eu-west-1.compute.amazonaws.com';
+      $dbname = "Team6";
+      $user = 'root';
+      $pass = 'code4good';
+      $DBH = new PDO("mysql:host=$host; dbname=$dbname", $user, $pass);
+      $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $e) {
+      echo 'Error connecting to database.';
+//      file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
   }
 
-  $sql = "INSERT INTO 'table'
-  		  VALUES ('$name','$email','$phone','$likes','$dislikes','$daysArray')";
+  try {
+      //added price. Make sure it matches column name in db
+      $STH = $DBH->prepare("INSERT INTO `volunteer` (`Username`, `Password`, `Address`, `Interests`, `Date`, `Black`, `White`) VALUES (:Username, :Password, :Address, :Interests, :Date, :Black, :White)");
+      $STH->bindParam(':Username', $username);
+      $STH->bindParam(':Password', $password);
+      $STH->bindParam(':Address', $address);
+      $STH->bindParam(':Interests', $interests);
+      $STH->bindParam(':Date', $date);
+      $STH->bindParam(':Black', $black);
+      $STH->bindParam(':White', $white);
 
-if(!$result = $db->query($sql)){
-  	die('Error running query: ' . $db->error);
-  	return false;
-  else{
-  	return true;
+      $username = $_POST['name'];
+      $password = $_POST['password'];
+      $address = $_POST['address'];
+      $interests = $_POST['interests'];
+      $white = $_POST['white'];
+      $black = $_POST['black'];
+      $date = gmdate("Y-m-d H:i:s");
+
+      if($STH->execute())
+      {
+        $insert = true;
+      }
+
+  } catch (PDOException $e) {
+        echo "Error submitting info to database";
+//        file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
+        exit;
   }
-}
 
-
-?>
+  if($insert)
+  {
+    echo 'Success! Thanks for registering';
+  }
+//  $days = implode(", ", $_POST['days']);
